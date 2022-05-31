f3e8992787b276bbbf7f78dca7f0251138180f32
Ivan Flores Rodriguez <i.flores@globant.com>
2022-05-30 22:22:19 -0500

    initial commit

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -6,11 +7,49 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
 
-  constructor() { }
+  miFormulario: FormGroup = this.formBuilder.group({
+    nombre: ['', [Validators.required, Validators.minLength(3)]],
+    favoritos: this.formBuilder.array([
+      ['Metal Gear', Validators.required],
+      ['Death Stranding', Validators.required]
+    ], Validators.required)
+  });
 
-  ngOnInit(): void {
+  a=1;
+
+  nuevoFavorito: FormControl = this.formBuilder.control('', Validators.required);
+
+  get favoritosArr(){
+    return this.miFormulario.get('favoritos') as FormArray;
+  }
+  constructor(private formBuilder: FormBuilder) { }
+
+  guardar(){
+    if(this.miFormulario.invalid){
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+    console.log( this.miFormulario.value);
+    this.miFormulario.reset();
+  }
+
+  campoEsValido(campo: string){
+    return this.miFormulario.controls[campo]?.errors && this.miFormulario.controls[campo]?.touched
+  }
+
+  agregarFavorito(){
+    if(this.nuevoFavorito.invalid){
+      return;
+    }
+
+    this.favoritosArr.push(new FormControl(this.nuevoFavorito.value, Validators.required));
+    this.nuevoFavorito.reset();
+  }
+
+  borrar(index: number){
+    this.favoritosArr.removeAt(index);
   }
 
 }
--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,4 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[]
+}
+
+interface Favorito {
+  id: number;
+  nombre: string;
+}
+
 
 @Component({
   selector: 'app-dinamicos',
@@ -6,11 +18,47 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
 
-  constructor() { }
+  @ViewChild('miFormulario') miFormulario!: NgForm;
 
-  ngOnInit(): void {
+  nuevoJuego: string = '';
+ 
+  persona: Persona =
+  {
+    nombre: 'Fernando',
+    favoritos: [
+      {
+        id: 1,
+        nombre: 'Metal Gear'
+      },
+      {
+        id: 2,
+        nombre: 'Death Stranding'
+      }
+    ]
   }
 
+  agregarJuego(){
+    const nuevoFavorito: Favorito = {
+      nombre: this.nuevoJuego,
+      id: this.persona.favoritos.length + 1
+    }
+
+    this.persona.favoritos.push({...nuevoFavorito});
+    this.nuevoJuego = '';
+  }
+
+  eliminar(index: number): void{
+    this.persona.favoritos.splice(index, 1);
+  }
+
+  guardar(){
+    console.log('formulario posteado');
+  }
+
+  formularioValido(): boolean{
+    return this.miFormulario?.controls['nombre']?.invalid &&
+      this.miFormulario?.controls['nombre']?.touched;
+  }
 }
